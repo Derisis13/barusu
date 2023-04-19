0583c242c22f895f7e6417a2bfbecf548de33cd6
Derisis13 <email.felhasznalasra@gmail.com>
2023-04-19 13:24:39 +0200

    fix: description in setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from distutils.core import setup
 import setuptools
 
 setup(name="barusu",
-      version='1.0', description="Python apt and dconf backup tool", author="Derisis13",
+      version='1.0', description="Python tool for apt, flatpak and dconf tool", author="Derisis13",
       py_modules=["main"],
       packages=setuptools.find_packages())
